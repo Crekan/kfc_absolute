20eb5e4838ba1898702a28238c207a54d201c894
vaniakazeko <vania.kazeko.00@bk.ru>
2023-05-11 11:47:28 +0300

    adding a redirect

--- backend/kfc_absolute/temporary/models.py
+++ backend/kfc_absolute/temporary/models.py
@@ -29,7 +29,7 @@ class Temporary(models.Model):
 
     day = models.CharField(max_length=20, choices=DAY_CHOICES)
     shift_type = models.CharField(max_length=30, choices=SHIFT_CHOICES)
-    custom_time = models.CharField(max_length=255)
+    custom_time = models.CharField(max_length=255, null=True, blank=True)
     date_add = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     night = models.BooleanField(default=False)
--- backend/kfc_absolute/temporary/views.py
+++ backend/kfc_absolute/temporary/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from rest_framework import generics
 
 from .models import Temporary
@@ -32,3 +34,9 @@ class TemporaryCreateView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        instance = self.serializer_class.Meta.model.objects.get(id=response.data['id'])
+        url = reverse('temporary_create')
+        return HttpResponseRedirect(url)
