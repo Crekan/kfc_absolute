079d267b07f980bf6a02c17a943d6e282d087304
vaniakazeko <vania.kazeko.00@bk.ru>
2023-05-21 11:08:23 +0300

    model Feedback

--- backend/kfc_absolute/feedback/models.py
+++ backend/kfc_absolute/feedback/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Feedback(models.Model):
+    TYPE_TREATMENT_CHOICES = (
+        ('жалоба', 'жалоба'),
+        ('Предложение/идея', 'Предложение/идея'),
+        ('Похвала', 'Похвала'),
+        ('Другое', 'Другое'),
+    )
+    FOR_WHOM_CHOICES = (
+        ('Весь управляющий состав (зам. директора и директор)', 'Весь управляющий состав (зам. директора и директор)'),
+        ('Весь менеджерский состав', 'Весь менеджерский состав'),
+        ('Весь состав тренеров', 'Весь состав тренеров'),
+        ('Весь состав ЧК', 'Весь состав ЧК'),
+        ('Всю команду', 'Всю команду'),
+        ('Другое', 'Другое'),
+    )
+
+    address = models.CharField(max_length=150, choices=TYPE_TREATMENT_CHOICES)
+    team = models.CharField(max_length=255, choices=FOR_WHOM_CHOICES)
+    situation = models.TextField()
+
+    def __str__(self):
+        return f'{self.address} - {self.team}'
+
